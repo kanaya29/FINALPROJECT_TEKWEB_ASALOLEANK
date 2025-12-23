a7c44d19b0479fe0929ba871d6bfd76ca54db05a
kanaya29 <kanayaailsa05@gmail.com>
2025-12-23 17:40:11 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Button>Shadcn Jalan</Button>
+    </div>
+  )
+}
+
+export default App
