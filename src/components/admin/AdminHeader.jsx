c29e68df6c45bbfb124758267d28084ac8c13485
kanaya29 <kanayaailsa05@gmail.com>
2026-01-04 16:16:55 +0700

    fitur admin selesai

--- src/components/admin/AdminHeader.jsx
+++ src/components/admin/AdminHeader.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+import { Menu } from "lucide-react"; 
 
 const AdminHeader = () => {
   return (
-    <div className="mb-4">
-      <button className="text-blue-600 hover:underline flex items-center text-sm">
-        <span className="mr-1">←</span> Kembali ke Home
-      </button>
+    <div className="bg-blue-500 border-b p-6 flex justify-between items-center shadow-sm">
+      <div className="flex text-white items-center gap-3">
+        <Link 
+          to="/" 
+          className="p-1 hover:bg-gray-200 rounded-lg transition-colors text-white"
+          title="Kembali ke Website User"
+        >
+          <Menu size={24} />
+        </Link>
+        <h2 className="font-bold text-xl text-white">Admin Dashboard</h2>
+      </div>
+      
+      <div className="text-sm text-xl text-white">
+        Hari ini: {new Date().toLocaleDateString()}
+      </div>
     </div>
   );
 };
